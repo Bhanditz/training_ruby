407cd84e52dcf9646e8d95a1226c04e6ef737829
Dmitry Mozzherin <dmozzherin@gmail.com>
2010-08-24 15:20:56 -0400

    lesson5 added

--- lesson4/README.rb
+++ lesson4/README.rb
@@ -4,12 +4,11 @@
 # 
 #    1. Object Oriented approach to programming 
 #    2. Ruby consists of objects and dots (the most-used syntactic structure) 
-#    3. "Everything is an expression" (vs statement), meaning everything returns 
+#    3. Introspection - in a console, pull up an object and show that everything can be examined.
+#    4. "Everything is an expression" (vs statement), meaning everything returns 
 #       something (there are no "void" methods).  For example,
 #       returns the value of the expression.
-#    4. Method chaining...  (come up with an example here - TODO)
-#    5. You can add methods to things dynamically - show an example of defining a method on something in a console
-#    6. Introspection - in a console, pull up an object and show that everything can be examined.
+#    5. Method chaining...  (come up with an example here - TODO)
 
 
 # == Object Oriented approach to programming
@@ -117,54 +116,3 @@ end + ' time'
 # Simple expressions return themselves, for example expression 4 returns 4
 # Everything can be chain to everything, but if a method is missing chain would break
 
-# = Blocks
-# Blocks change how you work with loops the most.
-# Blocks make your functions very flexible.
-# Flow control goes to blocks by using 'yield'
-def my_func
-  x = yield "Hi "
-  puts x + '!'
-  'method returns this string'
-end
-
-my_func {|str| str + ' world'}
-
-# blocks are pieces of code separated wither by { and } or by 'do' and 'end'
-# both syntax structures are almost identical, by convention {,} are used for one-liners, 'do', 'end' for multi-line blocks.
-
-#loops can be done in a traditional way
-for a in [1,2,3]
-  puts a
-end
-
-# Arrays, hashes, strings are things that can be traversed through -- they all are Enumerables. All Enumerables have method 'each'
-[1,2,3].each {|a| puts a} #uses block of 'each' method
-{'a' => 1, 'b' => 2}.each {|key,val| puts key}
-
-# Say you want to modify data in a Enumerator
-
-[1,2,3].map {|a| 'a' * a} # => ["a", "aa", "aaa"] 
-
-# Or you want to filter some data out
-['one', 'two', 'three'].select {|a| a.match /o/} # => ["one", "two"]
-
-# Or you want to split data into 2 'buckets'
-['one', 'two', 'three'].partition {|a| a.match /o/} # => [["one", "two"], ["three"]]
-
-# Or you can do cumulative things: 
-[1, 2, 3].inject(0) {|res, n| res += n} # => 6
-[1, 2, 3].inject({}) {|res, n| res[n] = 'a' * n; res} # => {1=>"a", 2=>"aa", 3=>"aaa"}
-
-
-require 'blocks'
-
-# = Dynamic nature of ruby
-# There is nothing sacred in Ruby...
-# Monkey patching is a common practice (somewhat dangerous)
-obj = 'hello'
-
-def obj.continue
-  self.to_s + ' world'
-end
-
-puts obj.continue
---
+++ lesson5/README.rb
@@ -0,0 +1,57 @@
+# vim: set spell:
+
+#    1. Blocks
+#    2. Introduction to Monkey Patching
+
+
+# = Blocks
+# Blocks change how you work with loops the most.
+# Blocks make your functions very flexible.
+# Flow control goes to blocks by using 'yield'
+def my_func
+  x = yield "Hi "
+  puts x + '!'
+  'method returns this string'
+end
+
+my_func {|str| str + ' world'}
+
+# blocks are pieces of code separated wither by { and } or by 'do' and 'end'
+# both syntax structures are almost identical, by convention {,} are used for one-liners, 'do', 'end' for multi-line blocks.
+
+#loops can be done in a traditional way
+for a in [1,2,3]
+  puts a
+end
+
+# Arrays, hashes, strings are things that can be traversed through -- they all are Enumerables. All Enumerables have method 'each'
+[1,2,3].each {|a| puts a} #uses block of 'each' method
+{'a' => 1, 'b' => 2}.each {|key,val| puts key}
+
+# Say you want to modify data in a Enumerator
+
+[1,2,3].map {|a| 'a' * a} # => ["a", "aa", "aaa"] 
+
+# Or you want to filter some data out
+['one', 'two', 'three'].select {|a| a.match /o/} # => ["one", "two"]
+
+# Or you want to split data into 2 'buckets'
+['one', 'two', 'three'].partition {|a| a.match /o/} # => [["one", "two"], ["three"]]
+
+# Or you can do cumulative things: 
+[1, 2, 3].inject(0) {|res, n| res += n} # => 6
+[1, 2, 3].inject({}) {|res, n| res[n] = 'a' * n; res} # => {1=>"a", 2=>"aa", 3=>"aaa"}
+
+
+require 'blocks'
+
+# = Introduction to Monkey Patching
+# There is nothing sacred in Ruby...
+# Monkey patching is a common practice (somewhat dangerous)
+obj = 'hello'
+
+def obj.continue
+  self.to_s + ' world'
+end
+
+puts obj.continue
